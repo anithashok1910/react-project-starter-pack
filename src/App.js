3dfce41482ef2ea110d8064819c89746f006646b
Anith Ashok <anith.ashok1910@gmail.com>
2019-01-15 01:23:10 +0530

    add login logout functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Routes from './components/Routes';
 import { BrowserRouter } from 'react-router-dom';
 import { Layout } from 'antd';
 import NavBar from './components/navBar/NavBar';
+import { ApplicationContextProvider } from './context/ApplicationContext';
 
 import './App.css';
 const { Content, Footer } = Layout;
@@ -10,21 +11,23 @@ const { Content, Footer } = Layout;
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Layout className="layout">
-          <NavBar />
-          <Content
-            style={{ padding: '0 50px', margin: '16px 0', height: '78vh' }}
-          >
-            <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
-              <Routes />
-            </div>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>
-            React Project Starter Pack
-          </Footer>
-        </Layout>
-      </BrowserRouter>
+      <ApplicationContextProvider>
+        <BrowserRouter>
+          <Layout className="layout">
+            <NavBar />
+            <Content
+              style={{ padding: '0 50px', margin: '16px 0', height: '78vh' }}
+            >
+              <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+                <Routes />
+              </div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>
+              React Project Starter Pack
+            </Footer>
+          </Layout>
+        </BrowserRouter>
+      </ApplicationContextProvider>
     );
   }
 }
